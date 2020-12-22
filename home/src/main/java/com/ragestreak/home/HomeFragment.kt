3b39ab01ccaa3261b9e831cebf551bc9072fd260
devLucasYuji <lucasyujideveloper@gmail.com>
2020-12-22 11:08:09 -0300

    create buttom custom view

--- home/src/main/java/com/ragestreak/home/HomeFragment.kt
+++ home/src/main/java/com/ragestreak/home/HomeFragment.kt
@@ -2,7 +2,7 @@ package com.ragestreak.home
 
 import com.ragestreak.commons.BaseFragment
 
-class HomeActivity: BaseFragment() {
+class HomeFragment: BaseFragment() {
 
     override val layoutRes: Int
         get() = R.layout.activity_home
---
+++ ragestreak-commons/src/main/java/com/ragestreak/commons/view/Button.kt
@@ -0,0 +1,45 @@
+package com.ragestreak.commons.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.ContextThemeWrapper
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatButton
+
+import com.ragestreak.commons.R
+
+class Button : AppCompatButton {
+
+    constructor(context: Context) : super(
+        ContextThemeWrapper(context, R.style.AppTheme_Button),
+        null,
+        R.style.AppTheme_Button
+    )
+
+    constructor(context: Context, attrs: AttributeSet) : super(
+        ContextThemeWrapper(context, R.style.AppTheme_Button),
+        attrs,
+        R.style.AppTheme_Button
+    )
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttrs: Int = R.style.AppTheme_Button
+    ) : super(
+        ContextThemeWrapper(context, R.style.AppTheme_Button),
+        attrs,
+        defStyleAttrs
+    )
+
+    init {
+        try {
+            isClickable = true
+            isFocusable = true
+        } finally {
+            invalidate()
+            requestLayout()
+        }
+    }
+}
